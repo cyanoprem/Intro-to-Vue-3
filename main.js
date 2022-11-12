c823bf40bb264b39f88b8bf522c0493b03dc438f
Cyano Prem <cyano.prem@gmail.com>
2022-11-12 12:15:02 +0530

    Complete

--- main.js
+++ main.js
@@ -11,5 +11,17 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        decrementCart() {
+            this.cart -= 1
+        },
+        showVariant(variantImage) {
+            // console.log(key);
+            this.image = variantImage;
+            }
     }
 })
